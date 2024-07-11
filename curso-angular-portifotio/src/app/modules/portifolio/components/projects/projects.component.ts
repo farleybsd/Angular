a53f30851362cc738a515008b81f6a3ecfb9e649
farleybsd <farley.t.i@hotmail.com>
2024-07-11 09:09:29 -0300

    Finish

---
+++ curso-angular-portifotio/src/app/modules/portifolio/components/projects/projects.component.ts
@@ -0,0 +1,44 @@
+import { Component, inject, signal } from '@angular/core';
+import { IProjects } from '../../interface/IProjects.interface';
+import {MatDialogModule,MatDialog} from '@angular/material/dialog';
+import { EDialogPanelClass } from '../../enum/EDialogPanelClass';
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
+
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss'
+})
+export class ProjectsComponent {
+
+  public arrayProjects = signal<IProjects[]>([
+    {
+      src: 'assets/img/projects/vfull.png',
+      alt: 'Projeto Vida FullStack',
+      title: 'Vida FullStack',
+      with: '100px',
+      height: '51px',
+      description:
+        '<p>Explore o fascinante mundo do desenvolvimento web no meu blog dedicado exclusivamente ao ecossistema Angular, Front-end, JavaScript, Node.js e TypeScript.</p> <p>Desvende os segredos por trás da construção de interfaces modernas, aprimore suas habilidades de programação front-end quanto back-end e mergulhe em conceitos avançados de JavaScript e TypeScript.</p>',
+      links: [
+        {
+          name: 'Conheça o Blog',
+          href: 'https://vidafullstack.com.br',
+        },
+      ],
+    },
+  ]);
+
+  #dialog = inject(MatDialog);
+
+  public openDialog(data: IProjects) {
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS,
+    });
+  }
+
+}
